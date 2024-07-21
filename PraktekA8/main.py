43dd4f51e11361285a274a6bbf776403bd8f23a6
Fadel <mfiva213@gmail.com>
2024-07-21 22:54:05 +0700

    Praktikum A5-A8

---
+++ PraktekA8/main.py
@@ -0,0 +1,150 @@
+import cv2
+import sys
+import numpy as np
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.uic import loadUi
+
+class ShowImage(QMainWindow):
+    def __init__(self):
+        super(ShowImage, self).__init__()
+        loadUi('showgui.ui', self)
+        self.image = None
+        self.loadButton.clicked.connect(self.loadClicked)
+        self.grayButton.clicked.connect(self.grayClicked)
+        self.actionOperasi_Pencerahan.triggered.connect(self.brightness)
+        self.actionSimple_Contrast.triggered.connect(self.contrast)
+        self.actionContrast_Streching.triggered.connect(self.contrastStreching)
+        self.actionNegative_Image.triggered.connect(self.negativeImage)
+        self.actionBiner_Image.triggered.connect(self.binerImage)
+
+    @pyqtSlot()
+    def loadClicked(self):
+        self.loadImage('Koala.jpg')
+
+    def loadImage(self, flname):
+        self.image = cv2.imread(flname)
+        self.displayImage()
+
+    def displayImage(self):
+        qformat = QImage.Format_Indexed8
+
+        if len(self.image.shape) == 3:  # row[0], col[1], channel[2]
+            if self.image.shape[2] == 4:
+                qformat = QImage.Format_RGBA8888
+            else:
+                qformat = QImage.Format_RGB888
+
+        img = QImage(self.image.data, self.image.shape[1], self.image.shape[0], self.image.strides[0], qformat)
+        img = img.rgbSwapped()
+
+        self.imgLabel.setPixmap(QPixmap.fromImage(img))
+        self.imgLabel.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+    def grayClicked(self):
+                H, W = self.image.shape[:2]
+                gray = np.zeros((H, W), np.uint8)
+                for i in range(H):
+                    for j in range(W):
+                        gray[i, j] = np.clip(
+                            0.299 * self.image[i, j, 0] + 0.587 * self.image[i, j, 1] + 0.114 * self.image[i, j, 2], 0,
+                            255)
+                self.image = gray
+                self.displayImage(2)
+
+    def brightness(self):
+        try:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        except:
+            pass
+
+        H, W = self.image.shape[:2]
+        brightness = 50
+        for i in range(H):
+            for j in range(W):
+                a = self.image.item(i, j)
+                b = np.clip(a + brightness, 0, 255)
+                self.image.itemset((i, j), b)
+
+        self.displayImage(1)
+
+    def contrast(self):
+        # try:
+        #     self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        # except:
+        #     pass
+
+        H, W = self.image.shape[:2]
+        contrast = 1.6
+        for i in range(H):
+            for j in range(W):
+                a = self.image.item(i, j)
+                b = np.clip(a * contrast, 0, 255)
+                self.image.itemset((i, j), b)
+
+        self.displayImage(1)
+
+    def contrastStreching(self):
+        try:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        except:
+            pass
+
+        H, W = self.image.shape[:2]
+        minV = np.min(self.image)
+        maxV = np.max(self.image)
+
+        for i in range(H):
+            for j in range(W):
+                a = self.image.item(i, j)
+                b = float(a - minV) / (maxV - minV) * 255
+                self.image.itemset((i, j), b)
+
+        self.displayImage(1)
+
+    def negativeImage(self):
+        if self.image is not None:
+            negative_img = 255 - self.image
+            self.image = negative_img
+            self.displayImage()
+
+    def binerImage(self):
+        if self.image is not None:
+            try:
+                self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+            except:
+                pass
+
+            ret, binary_image = cv2.threshold(self.image, 127, 255, cv2.THRESH_BINARY)
+
+            self.image = binary_image
+            self.displayImage(1)
+
+
+    def displayImage(self, window=1):
+        qformat = QImage.Format_Indexed8
+        if len(self.image.shape) == 3:
+            if self.image.shape[2] == 4:
+                qformat = QImage.Format_RGBA8888
+            else:
+                qformat = QImage.Format_RGB888
+        img = QImage(self.image, self.image.shape[1], self.image.shape[0], self.image.strides[0], qformat)
+        img = img.rgbSwapped()
+
+        if window == 1:
+            self.imgLabel.setPixmap(QPixmap.fromImage(img))
+            self.imgLabel.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+            self.imgLabel.setScaledContents(True)
+
+        if window == 2:
+            self.hasilLabel.setPixmap(QPixmap.fromImage(img))
+            self.hasilLabel.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+            self.hasilLabel.setScaledContents(True)
+
+app = QtWidgets.QApplication(sys.argv)
+window = ShowImage()
+window.setWindowTitle('Show Image GUI')
+window.show()
+sys.exit(app.exec_())
